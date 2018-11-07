af76988d6695a9cd8c9202bd845c39d6938997e3
Alona <dorozhynskaad@gmail.com>
2018-11-07 16:00:46 +0200

    Buttons

--- src/components/creditInfo/creditInfo.js
+++ src/components/creditInfo/creditInfo.js
@@ -2,6 +2,23 @@ import React, {Component} from 'react';
 
 import './creditInfo.css';
 class CreditInfo extends Component {
+
+    state = {
+        name: 'Billi',
+        surname:'Billigan',
+        amount:10,
+        term:10
+    }
+  
+    inputHandler = (event) => {
+        event.preventDefault();
+        this.setState({
+            name: this.refs.Name.value,
+            surname:this.refs.Surname.value,
+            amount:this.refs.Amount.value,
+            term: this.refs.Term.value,
+        })
+    }
     render() {
         return ( 
         < div className = 'AllCreditInfo' >
@@ -35,46 +52,44 @@ class CreditInfo extends Component {
                 
                             <div className="form-group">
                                 <label>Client name</label>
-                                <input type="text" className="form-control" name="clientName" placeholder=" Enter Name" required/>
+                                <input type="text" ref = {'Name'}  className="form-control"  placeholder=" Enter Name" required/>
                                 </div>
                             <div className="form-group">
                                 <label>Client surname</label>
-                                <input type="text" classname="form-control" name="clientSurname"
-                                    placeholder=" Enter client surname" required/>
+                                <input type="text" ref = {'Surname'}  classname="form-control" placeholder=" Enter client surname" required/>
                             </div>
                             <div className="form-group">
                                 <label>Amount</label>
-                                <input type="number" className="form-control" name="amount" placeholder=" Enter amount" required/>
+                                <input type="number" ref = {'Amount'}  className="form-control"  placeholder=" Enter amount" required/>
                             </div>
                             <div className="form-group">
                                 <label>Term</label>
-                                <input type="number" className="form-control" maxlength="2" min="3" max="12" name="term"
-                                    placeholder=" Enter term" required/>
+                                <input type="number" ref ={'Term'}  className="form-control" maxlength="2" min="3" max="12" name="term" placeholder=" Enter term" required/>
                             </div>
                     <div>
 
-                        <button type="button" className=" submit" name="submit"><i className="fa fa-paper-plane" aria-hidden="true"></i>
-                                    Send
-                        </button>
+                        <button type="button" className="submit" onClick = {this.inputHandler}>Send </button>
                     </div>
                
-                    < div className = "form-group scroll backscroll" >
+                    < div className = "form-group  backscroll" >
                         <table className="table">
-                            <thead>
-                            <th>Name</th>
+                            <thead> <th>Name</th>
                             <th>Surname</th>
                             <th>Amount</th>
                             <th>Term</th>
+                           
                             </thead>
                             <tbody id="creditList">
-
+ <tr>
+                            <td>{ this.state.name}</td>
+                            <td>{ this.state.surname}</td>
+                            <td>{ this.state.amount}</td>
+                            <td>{ this.state.term}</td>
+                            </tr>
                             </tbody>
                         </table>
 
                      </div>
-                
-                       
-
                     </div>
                 
             
--- src/components/registration/registration.js
+++ src/components/registration/registration.js
@@ -2,17 +2,36 @@ import React, {Component} from 'react';
 
 import './registration.css';
 class Registration extends Component {
+    state = {
+    login: 'Login',
+    pussword: 'Pussword'
+}
+// changeTitle =(event)=>{
+//     console.log(event.target.value)
+//     this.setState({
+//         title: event.target.value
+//     })
+// }
+ inputValue= (event)=>{
+    
+this.setState({
+    login: this.refs.Login.value,
+    pussword: this.refs.Pussword.value
+})
+}
     render() {
         return ( 
            <div className = "login-section">
            <div className = 'container'>  
                     <p> Login </p> 
-                    <input  className = "login" name = "login"/>
+                    <input ref = {'Login'} className = "login" name = "login"/>
                     <p> Password </p> 
-                    <input  className = "password" name = "password"/> <br/>
-                    <input type = "button" value = "Login" id = "login" className = "btn-login"/> 
+                    <input ref = {'Pussword'} className = "password" name = "password"/> <br/>
+                    <input type = "button" onClick = {this.inputValue} value = "Login" id = "login" className = "btn-login"/> 
+
+                    <h1>{this.state.login} {this.state.pussword}</h1>
               </div>
-               <p className = "showFalse" > </p>
+               <p  className = "showFalse" > </p>
                <hr/>
            </div>
                
